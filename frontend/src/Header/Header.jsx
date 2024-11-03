f8e2b865c0ca10aa1ec60ec51983777c89a27c42
HungHoang1 <2051063705@e.tlu.edu.vn>
2024-11-03 15:46:28 +0700

    khoi tao du an + code phan Header

---
+++ frontend/src/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Ẩm thực Hà Nội</h2>
+            <p>Ẩm thực Hà Nội là nét văn hóa đặc sắc, giàu truyền thống, phản ánh sự tinh tế và hài hòa trong cách chế biến. Các món nổi tiếng như phở, bún chả, chả cá Lã Vọng và bánh cuốn không chỉ có hương vị đậm đà mà còn mang đến trải nghiệm văn hóa khi thưởng thức trong không gian quán nhỏ hay gánh hàng rong. Đây chính là bản sắc riêng, tạo nên một Hà Nội thanh lịch và gần gũi.</p>
+          <button>Các món ăn nên thử ở Hà Nội</button> 
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+    const [menu , setMenu] = useState("home");
+  return (
+    <div className='Navbar'>
+      <img src={assets.logo} alt=""  className='logo'/>
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Trang chủ</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        </ul>
+      <button>Đăng nhập</button>
+    </div>
+  )
+}
+
+export default Navbar
